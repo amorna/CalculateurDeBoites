01efeb7e47dba1aa015550fd1ca3d10dce9d4cd0
amorna <amorna2@gmail.com>
2024-05-26 22:38:03 +0100

    correction police et clavier

--- app/src/main/java/com/amorna/calculateurdeboites/MainActivity.java
+++ app/src/main/java/com/amorna/calculateurdeboites/MainActivity.java
@@ -1,12 +1,14 @@
 package com.amorna.calculateurdeboites;
 
 import android.annotation.SuppressLint;
+
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -14,9 +16,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText jour,periode,contenance;
+    private EditText jour, periode, contenance;
     private TextView nombre;
-    private Button btn,reset;
+    private Button btn, reset;
+
+
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,23 +32,22 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        jour=findViewById(R.id.jour);
-        periode=findViewById(R.id.periode);
-        contenance=findViewById(R.id.contenance);
-        nombre=findViewById(R.id.nombre);
-        btn=findViewById(R.id.btn);
-        reset=findViewById(R.id.reset);
+        jour = findViewById(R.id.jour);
+        periode = findViewById(R.id.periode);
+        contenance = findViewById(R.id.contenance);
+        nombre = findViewById(R.id.nombre);
+        btn = findViewById(R.id.btn);
+        reset = findViewById(R.id.reset);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(jour.length()==0){
-                  jour.setError("Entrer le nombre d'unités par jour");
-                } else if (periode.length()==0) {
+                if (jour.length() == 0) {
+                    jour.setError("Entrer le nombre d'unités par jour");
+                } else if (periode.length() == 0) {
                     periode.setError("Entrer la periode en jours");
-                } else if (contenance.length()==0) {
+                } else if (contenance.length() == 0) {
                     contenance.setError("Entrer la contenance de la boîte");
-                }
-                else {
+                } else {
 
 
                     double j = Double.parseDouble(String.valueOf(jour.getText()));
@@ -53,6 +56,9 @@ public class MainActivity extends AppCompatActivity {
                     double nb1 = j * p / c;
                     int nb = (int) Math.ceil(nb1);
                     nombre.setText(String.valueOf(nb) + " boîtes");
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+                    imm.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0);
+
                 }
 
             }
